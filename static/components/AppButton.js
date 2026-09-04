fe1146679b4fbf7892a5c4bb2d28b3288324ffe5
424854 宮脇将 <424854@m.mie-u.ac.jp>
2026-09-04 21:24:09 +0900

    Vue.jsの仕様確認

--- static/components/AppButton.js
+++ static/components/AppButton.js
@@ -1,12 +1,27 @@
-export default {
-  name: 'AppButton',
-  props: {
-    variant: { type: String, default: 'primary' },
-  },
-  emits: ['click'],
-  template: `
-    <button type="button" class="btn" :class="'btn-' + variant" @click="$emit('click')">
-      <slot></slot>
-    </button>
-  `,
+const name = 'AppButton';
+
+const props = {
+  variant: { type: String, default: 'primary' },
+  disabled: { type: Boolean, default: false },
 };
+
+const emits = ['press'];
+
+// クリックイベントを受け取り、disabled状態を確認したうえで'press'を発火する
+function setup(props, { emit }) {
+  function handleClick(event) {
+    if (props.disabled) return;
+
+    emit('press', event);
+  }
+
+  return { handleClick };
+}
+
+const template = `
+  <button type="button" class="btn" :class="'btn-' + variant" :disabled="disabled" @click="handleClick">
+    <slot></slot>
+  </button>
+`;
+
+export default {name, props, emits, template, setup};
